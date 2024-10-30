86d451d722577fe3127fe68ce1fbbb07aa42e3c1
Corvinus <y2344zha@uwaterloo.ca>
2024-10-29 23:40:30 -0400

    add test route

--- src/server/http/http_server.rs
+++ src/server/http/http_server.rs
@@ -1,4 +1,17 @@
-use rocket::{get, routes};
+use rocket::{get, post, routes};
+use rocket::serde::Deserialize;
+use rocket::serde::json::Json;
+
+#[derive(Deserialize)]
+struct MyData {
+    message: String,
+}
+
+#[post("/echo", data = "<data>")]
+async fn json_handler(data: Json<MyData>) -> String {
+    let data = data.into_inner(); // Get the actual `MyData` struct
+    format!("Received message: {}", data.message)
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -6,5 +19,5 @@ fn index() -> &'static str {
 }
 
 pub async fn init() {
-    let rocket = rocket::build().mount("/", routes![index]).launch().await;
+    let rocket = rocket::build().mount("/", routes![index, json_handler]).launch().await;
 }
